a0b8a3222634568b5a53dcb61ba74838bcdf95ad
Natalya Pchelintseva <pchelintseva.n@yandex.ru>
2022-01-27 18:23:49 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        Scanner scanner = new Scanner(System.in);
+
+        s.setName(scanner);
+        s.setMarks(scanner);
+        s.printData();
+        System.out.println("Average mark is " + s.getAverageMark());
+        System.out.println(s);
+    }
+
+}
---
+++ src/Student.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Student {
+
+    private String name;
+    private int[] marks = new int[5];
+
+    public void setName(Scanner s) {
+        System.out.print("Enter student name: ");
+        if (s.hasNextLine()) {
+            name = s.nextLine();
+        }
+    }
+
+    public void setMarks(Scanner s) {
+        System.out.println("Enter student's marks:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("-> ");
+            marks[i] = s.nextInt();
+            s.hasNextLine();
+        }
+    }
+
+    public void printData() {
+        String data = "name: " + name + "\nmarks: ";
+        for (int i = 0; i < 5; i++) {
+            data += marks[i] + " ";
+        }
+        System.out.println(data);
+    }
+
+    public float getAverageMark() {
+        float sum = 0f;
+        for (int i = 0; i < 5; i++) {
+            sum += marks[i];
+        }
+        return sum / 5;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", marks=" + Arrays.toString(marks) +
+                '}';
+    }
+}
